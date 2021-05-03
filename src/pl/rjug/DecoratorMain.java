e3c1dd829335b13dc9fb6137c504b424497ed5a8
PaniOdProgramowania <pani.od.programowania@gmail.com>
2021-05-03 19:23:47 +0200

    Add decorator p2

--- src/pl/rjug/DecoratorMain.java
+++ src/pl/rjug/DecoratorMain.java
@@ -7,17 +7,21 @@ import java.io.IOException;
 public class DecoratorMain {
 
     public static void main(String[] args) {
-        char[] result = new char[Integer.MAX_VALUE];
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("file/very_long_file.txt"));
-            long startTime = System.nanoTime();
-            int nrOfCharsRead = bufferedReader.read(result, 0, Integer.MAX_VALUE);
-            long endTime = System.nanoTime();
-            System.out.println("Przeczytano: " + nrOfCharsRead);
-            System.out.println(endTime - startTime);
+        for (int i = 0; i < 10; i++) {
+            int nrOfChars = 51490;
+            char[] result = new char[nrOfChars];
+            try {
+//            FileReader bufferedReader = new FileReader("file/very_long_file.txt");
+                BufferedReader bufferedReader = new BufferedReader(new FileReader("file/very_long_file.txt"));
+                long startTime = System.nanoTime();
+                int nrOfCharsRead = bufferedReader.read(result, 0, nrOfChars);
+                long endTime = System.nanoTime();
+                System.out.println("Przeczytano: " + nrOfCharsRead);
+                System.out.println(endTime - startTime);
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
